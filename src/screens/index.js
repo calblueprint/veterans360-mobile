cdddca99abc28395d58f8c1fca0241e64eb780f2
Ken Chen <lbkchen@gmail.com>
2017-10-17 22:00:44 -0700

    Remove unnecessary files from previous navigator lib.

--- src/screens/index.js
+++
@@ -1,8 +0,0 @@
-import { Navigation } from 'react-native-navigation';
-
-import TestScreen from './TestScreen';
-
-
-export function registerScreens() {
-  Navigation.registerComponent('test.TestScreen', () => TestScreen);
-}
