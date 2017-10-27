07f84deecc2feac7856974b353ea5b1eafc5db36
Ken Chen <lbkchen@users.noreply.github.com>
2017-10-26 17:00:08 -0700

    Adds login component and forms and submission button and UI components (#7)
    * Adds base login screen, base background component, base raised container, and forms library.
    
    * Add Form wrapper component for tcomb Forms.
    
    * Adds more form styling and finishes login screen style.
    
    * Add secure text entry to field.
    
    * Add some touches to styling and fix icon direction.
    
    * Add some documentation and add sign up button.
    
    * Adds more docs.

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,128 @@
+/**
+ * Login screen for veterans. 
+ */
+
+import React from 'react';
+import Icon from '@expo/vector-icons/FontAwesome';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { Form, t } from '../components/Form';
+
+import { imageStyles } from '../styles/images';
+import { layoutStyles, marginTop } from '../styles/layout';
+import { colors } from '../styles/colors';
+import BackgroundOverlay from '../components/BackgroundOverlay';
+import RaisedContainer from '../components/RaisedContainer';
+import Button from '../components/Button';
+
+export default class LoginScreen extends React.Component {
+
+  static navigationOptions = {
+    tabBarLabel: 'Login',
+    tabBarIcon: ({ tintColor }) => (
+      <Icon name="check" size={20} color="#e91e63" />
+    ),
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.login = this.login.bind(this);
+    this.redirectToSignupScreen = this.redirectToSignupScreen.bind(this);
+  }
+
+  login(event, onSuccess, onFailure) {
+    event.preventDefault();
+    const value = this.form.getValue();
+    if (value) {
+      console.log(value);
+      onSuccess && onSuccess(value);
+    } else {
+      console.error("Error occurred.");
+      onFailure && onFailure();
+    }
+  }
+
+  redirectToSignupScreen(event, onSuccess, onFailure) {
+    event.preventDefault();
+    // TODO: FILL IN
+  }
+
+  render() {
+    return (
+      <BackgroundOverlay>
+        <RaisedContainer style={styles.raisedContainer}>
+          <Text style={styles.titleStyle}>Login</Text>
+          <View style={styles.formContainer}>
+            <Form
+              style={styles.formStyle}
+              refCallback={(ref) => this.form = ref}
+              type={t.struct({
+                email: t.String,
+                password: t.String,
+              })}
+              options={{
+                fields: {
+                  password: { secureTextEntry: true },
+                },
+              }}
+            />
+            <Button
+              style={marginTop.md}
+              onPress={this.login}
+              text="SUBMIT"
+            />
+          </View>
+          <Button
+            style={styles.signupButtonStyle}
+            textStyle={styles.signupButtonTextStyle}
+            onPress={this.login}
+            text="SIGN UP"
+          />
+        </RaisedContainer>
+      </BackgroundOverlay>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  raisedContainer: {
+    position: 'absolute',
+    top: '30%',
+    left: '10%',
+    width: '80%',
+    height: '40%',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  formContainer: {
+    width: '100%',
+    justifyContent: 'center',
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 40,
+    paddingRight: 40,
+  },
+  formStyle: {
+    width: 500,
+  },
+  titleStyle: {
+    position: 'absolute',
+    top: -72,
+    color: colors.white,
+    backgroundColor: 'transparent',
+    fontSize: 36,
+    fontWeight: '300',
+    zIndex: 100,
+  },
+  signupButtonStyle: {
+    position: 'absolute',
+    bottom: -72,
+    height: 32,
+    borderRadius: 16,
+  },
+  signupButtonTextStyle: {
+    fontSize: 13,
+  },
+});
---
+++ src/styles/colors.js
@@ -0,0 +1,35 @@
+colors = {
+  // 5 grays from darkest to lightest
+  charcoal          : '#404040',
+  light_charcoal    : '#686868',
+  gray              : '#949494',
+  light_gray        : '#CBCBCB',
+  snow              : '#EDEDED',
+  light_snow        : '#F6F6F6',
+
+  // 5 blue-tinted grays from darkest to lightest
+  metal             : '#515A63',
+  light_metal       : '#6F7B86',
+  steel             : '#8C9BA9',
+  light_steel       : '#B8C4D0',
+
+  // Green
+  green             : '#18B671',
+  light_green       : '#4BF4AA',
+  dark_green        : '#15955D',
+
+  // Blue
+  blue              : '#25B0D6',
+  light_blue        : '#61D8F9',
+  dark_blue         : '#188DAD',
+
+  // Red
+  red               : '#D2475A',
+  light_red         : '#F8475E',
+  dark_red          : '#A33543',
+
+  // White
+  white             : '#FFFFFF',
+}
+
+export { colors };
