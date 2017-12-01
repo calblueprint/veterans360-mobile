aa6710a3442059884cad0f37ff9305bab941d1c7
Ken Chen <lbkchen@gmail.com>
2017-11-30 16:54:00 -0800

    Adds basic templace for profile gallery

---
+++ src/components/ProfileGallery.js
@@ -0,0 +1,17 @@
+/**
+ * Simple horizontal scrollable profile gallery shown on the
+ * Home Screen.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, Text, View } from 'react-native';
+import { colors } from '../styles/colors';
+
+export default class ProfileGallery extends React.Component {
+
+  render() {
+
+  }
+
+}
